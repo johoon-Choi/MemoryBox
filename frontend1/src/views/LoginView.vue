e34986628da050339ec9d3a8104953c7e9f706dc
johon-Choi <johon4687@gmail.com>
2023-10-10 17:14:03 +0900

    [ADD] Make LoginView.vue

---
+++ frontend1/src/views/LoginView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="login-container">
+    <a>gello</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LoginView"
+}
+</script>
+
+<style scoped>
+  .login-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%; /* 화면 가로폭을 꽉 채우도록 */
+    height: 100vh; /* 화면 세로 높이를 꽉 채우도록 */
+  }
+</style>
