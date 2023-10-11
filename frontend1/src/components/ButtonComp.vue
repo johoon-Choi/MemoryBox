05704b00ce1a96dbd27967b5040c22f0fca54aff
johon-Choi <johon4687@gmail.com>
2023-10-11 13:57:27 +0900

    [ADD] ButtonComp, TestView

---
+++ frontend1/src/components/ButtonComp.vue
@@ -0,0 +1,59 @@
+<template>
+  <div
+    :class="{
+      'button-container__default' : buttonType === 0,
+      'button-container__white' : buttonType === 1
+    }"
+  >
+    {{ buttonContent }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ButtonComp",
+  props: {
+    buttonType: {
+      type: Number,
+      required: false,
+      default: 0
+    },
+    buttonContent: {
+      type: String,
+      required: true,
+    },
+  },
+  methods: {
+    clickBtn() {
+      this.$emit('btnClicked');
+    },
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .button-container {
+    display: inline-block;
+    padding: 4px;
+
+    &__default {
+      background-color: rgba(142, 142, 142, 1.0);
+      border: 1px solid rgba(217, 217, 217, 1.0);
+
+      font-size: 8px;
+      color: rgba(255, 255, 255, 1.0);
+
+      border-radius: 4px;
+    }
+
+    &__default:hover {
+      background-color: rgba(166, 166, 166, 1.0);
+      border: 1px solid rgba(100, 100, 100, 1.0);
+
+      font-size: 8px;
+      color: rgba(0, 0, 0, 1.0);
+
+      border-radius: 4px;
+    }
+  }
+</style>
---
+++ frontend1/src/views/TestView.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="test-container">
+    <div class="test-container__part">
+      <a class="test-container__part__title"> Button Types</a>
+      <ButtonComp button-content="default button"></ButtonComp>
+    </div>
+  </div>
+</template>
+
+<script>
+import ButtonComp from "@/components/ButtonComp.vue";
+export default {
+  name: "TestView",
+  components: {ButtonComp}
+}
+</script>
+
+<style lang="scss" scoped>
+  .test-container {
+    margin: 8px;
+    display: flex;
+    flex-direction: column;
+
+    &__part {
+      display: flex;
+      flex-direction: column;
+
+      &__title {
+        display: flex;
+        font-size: 12px;
+        margin: 0 0 4px 0;
+      }
+    }
+  }
+</style>
